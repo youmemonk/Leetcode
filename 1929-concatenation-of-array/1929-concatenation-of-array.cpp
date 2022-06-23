3dca48449aa10b4636431a8805e00d30be320900
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-06-23 21:51:09 +0530

    Time: 7 ms (94.04%), Space: 12.9 MB (38.43%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        for(int i=0; i<2*n; i++){
+            ans.push_back(nums[(n+i)%n]);   
+        }
+        
+        return ans;
+    }
+};
