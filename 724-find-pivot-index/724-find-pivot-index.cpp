2590b4e3a68d12cc80d2ad88227477443a7f1576
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-06-29 03:06:44 +0530

    Time: 46 ms (30.51%), Space: 31.2 MB (54.67%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -4,14 +4,15 @@ public:
         int n = nums.size();
         int leftSum = 0, rightSum = 0;
         int total = 0;
-        for(int i:nums) total += i;
+        
+        for(int x:nums) total += x;
         
         for(int i=0; i<n; i++){
             if(i > 0){
                 leftSum += nums[i-1];
             }
-            
             rightSum = total - leftSum - nums[i];
+            
             if(leftSum == rightSum) return i;
         }
         
