14269528872ddf8d4e8626fdb9383ad0cc675ae6
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-06-30 00:27:30 +0530

    Time: 24 ms (89.12%), Space: 17.2 MB (31.40%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
@@ -1,12 +1,23 @@
 class NumArray {
 public:
     vector<int> nums;
+    vector<int> ps;
     NumArray(vector<int>& nums) {
+        int n = nums.size();
         this->nums = nums;
+        ps.resize(n);
+        prefixSumCalc(nums);
     }
     
     int sumRange(int left, int right) {
-        return accumulate(nums.begin() + left, nums.begin() + right + 1, 0);
+        // return accumulate(nums.begin() + left, nums.begin() + right + 1, 0);
+        return (left-1>=0) ? ps[right] - ps[left-1] : ps[right];
+    }
+    
+    void prefixSumCalc(vector<int> nums){
+        for(int i=0; i<nums.size(); i++){
+            (i==0) ? ps[i] = nums[i] : ps[i] = ps[i-1] + nums[i];
+        }
     }
 };
 
