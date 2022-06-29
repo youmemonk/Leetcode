ec4a5abeab871bdb513b64f0b4e8f668b3147c44
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-06-29 19:06:41 +0530

    Time: 323 ms (6.27%), Space: 103.2 MB (31.94%) - LeetHub

--- 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.cpp
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int waysToMakeFair(vector<int>& nums) {
-        int count = 0;
         int n = nums.size();
+        int count = 0;
         int totalEven = 0, totalOdd = 0;
-        
-        int psEven[n];
-        int psOdd[n];
+        vector<int> psEven(n);
+        vector<int> psOdd(n);
         
         psEven[0] = nums[0];
         psOdd[0] = 0;
-        
         for(int i=1; i<n; i++){
             if(i % 2 == 0){
                 psEven[i] = psEven[i-1] + nums[i];
@@ -22,10 +20,15 @@ public:
             }
         }
         
-        if(psOdd[n-1] - psOdd[0] == psEven[n-1] - psEven[0]) count++;
-        for(int i=1; i<n; i++){
-            totalEven = psEven[i-1] + psOdd[n-1] - psOdd[i];
-            totalOdd = psOdd[i-1] + psEven[n-1] - psEven[i];
+        for(int i=0; i<n; i++){
+            if(i == 0){
+                totalEven = psOdd[n-1] - psOdd[i];
+                totalOdd = psEven[n-1] - psEven[i];
+            }
+            else{
+                totalEven = psEven[i-1] + psOdd[n-1] - psOdd[i];
+                totalOdd = psOdd[i-1] + psEven[n-1] - psEven[i];
+            }
             
             if(totalEven == totalOdd) count++;
         }
