6a1c517128086a8952e06a0f0cb6d790e045f44d
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-06-29 23:39:13 +0530

    Time: 89 ms (60.46%), Space: 42.3 MB (97.02%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,40 +1,20 @@
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
+        //sliding window approach
         int n = cardPoints.size();
-        vector<int> psl(n);
-        vector<int> psr(n);
-        int maxVal = INT_MIN;
+        int i=0, j=n-k-1, total = 0, c = 0;
+        total = accumulate(cardPoints.begin()+j+1, cardPoints.end(), 0);
+        c = total;
+        i++; j++;
         
-        psl[0] = cardPoints[0];
-        for(int i=1; i<n; i++){
-            psl[i] = psl[i-1] + cardPoints[i];
+        while(j<n){
+            c -= cardPoints[j];
+            c += cardPoints[i-1];
+            i++; j++;
+            total = max(total, c);
         }
         
-        psr[n-1] = cardPoints[n-1];
-        for(int i=n-2; i>=0; i--){
-            psr[i] = psr[i+1] + cardPoints[i];
-        }
-        
-        for(int i=0; i<=k; i++){
-            int left, right;
-            if(i == 0){
-                left = 0;
-                right = psr[n-k];
-            }
-            else if(i > (k-1)){
-                left = psl[i-1];
-                right = 0;
-            }
-            else{
-                left = psl[i-1];
-                right = psr[n-k+i];
-            }
-            
-            int total = left + right;
-            maxVal = max(maxVal, total);
-        }
-        
-        return maxVal;
+        return total;
     }
 };
