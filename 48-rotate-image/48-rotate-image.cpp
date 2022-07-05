8ca250d902bc53bf27984e6160d26f528456e924
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-07-06 03:55:55 +0530

    Time: 7 ms (16.80%), Space: 6.9 MB (92.52%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        //flip across diagonal
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(j >= i)
+                    swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        //flip vertical
+        for(int i=0; i<n; i++){
+            int s = 0, e = n - 1;
+            while(s < e){
+                swap(matrix[i][s], matrix[i][e]);
+                s++;
+                e--;
+            }
+        }
+    }
+};
