f29f8dcb34ba70d89b39f212e867804ec74b4fbe
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-07-07 01:57:28 +0530

    Time: 25 ms (37.95%), Space: 13.2 MB (81.77%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        // TC: O(M*N) SC: O(M+N)
+        vector<int> rows;
+        vector<int> cols;
+        
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[0].size(); j++){
+                if(matrix[i][j] == 0){
+                    rows.push_back(i);
+                    cols.push_back(j);
+                }
+            }
+        }
+        
+        for(int row : rows){
+            for(int j=0; j<matrix[0].size(); j++){
+                matrix[row][j] = 0;
+            }
+        }
+        
+        for(int col : cols){
+            for(int i=0; i<matrix.size(); i++){
+                matrix[i][col] = 0;
+            }
+        }
+    }
+};
