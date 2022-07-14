e7324305a9bc1ccac47203b641a04cd4d3601eca
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-07-15 00:42:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.93%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -3,9 +3,10 @@ public:
     int hammingWeight(uint32_t n) {
         int count = 0;
         while(n > 0){
-            n = n & (n-1);
-            count++;
+            if(n & 1) count++;
+            n = n >> 1;
         }
+        
         return count;
     }
 };
