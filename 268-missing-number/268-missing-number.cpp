d1ade8a995bec3f9ab09d53df289f7983b74f1ec
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-07-15 01:30:26 +0530

    Time: 33 ms (38.85%), Space: 18 MB (18.76%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        unordered_set<int> s;
-        int n = nums.size();
-        for(int num : nums) s.insert(num);
-        
-        int i = 0;
-        while(i <= n){
-            if(s.find(i) == s.end()) return i;
-            i++;
+        //bit manipulation
+        int ans = 0, i = 0;
+        for(int num : nums){
+            ans = ans ^ num ^ i++;
         }
-        
-        return -1;
+        return (ans ^ i);
     }
 };
