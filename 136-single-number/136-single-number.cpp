f23476c6c06265a50361db33416c229c85734eec
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-07-15 01:22:41 +0530

    Time: 25 ms (72.89%), Space: 16.9 MB (85.96%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ans = 0;
-        for(int num : nums) ans ^= num;
-        
+        for(int num : nums) ans = ans ^ num;
         return ans;
     }
 };
