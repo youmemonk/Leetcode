526e8d0be3c2f7d801dbb62892567ebab50c64e1
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-07-16 01:44:31 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (26.95%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return (pow(a,2) - pow(b,2)) / (a - b);
+        unsigned carry;
+        while(b){
+            carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        
+        return a;
     }
 };
