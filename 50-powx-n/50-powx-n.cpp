a6a25896cb2a2b40506aae594821149b4490f272
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-07-21 23:29:11 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (43.20%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+        if(x == 1) return 1;
         if(n == 0) return 1;
-        else if(n < 0){
+        if(n < 0){
             x = 1/x;
             n = abs(n);
         }
         
-        double subp = myPow(x*x, n/2);
-        return (n&1) ? x * subp : subp;
+        double p = myPow(x*x, n/2);
+        if(n % 2 == 0) return p;
+        else return x*p;
     }
 };
