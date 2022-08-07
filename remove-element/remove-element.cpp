a6bb8237ff3b91b55c190295837a55e9248fd11e
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-08-08 00:32:14 +0530

    Time: 0 ms, Memory: 8.9 MB - LeetHub

--- remove-element/remove-element.cpp
+++ remove-element/remove-element.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        return distance(nums.begin(), remove(nums.begin(), nums.end(), val));
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == val) count++;
+            else nums[i - count] = nums[i];
+        }
+        
+        return (nums.size() - count);
     }
 };
