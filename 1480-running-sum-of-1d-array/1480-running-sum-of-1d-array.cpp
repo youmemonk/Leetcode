9108a78989be8debf1db7d19843dd6919a68caf8
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-08-09 01:51:06 +0530

    Time: 12 ms (6.87%), Space: 8.5 MB (37.13%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int> ps;
-        int n = nums.size();
-        
-        ps.push_back(nums[0]);
-        for(int i=1; i<n; i++){
-            int x = ps[i-1] + nums[i];
-            ps.push_back(x);
+        vector<int> ps(nums.size());
+        for(int i = 0; i < nums.size(); i++){
+            (i == 0) ? ps[i] = nums[i] : ps[i] = ps[i-1] + nums[i];
         }
         
         return ps;
