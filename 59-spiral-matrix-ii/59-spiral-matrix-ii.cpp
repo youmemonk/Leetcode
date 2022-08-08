742c6a6f0f87097280f8409d8cacd4c0f12663ee
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-08-09 00:56:45 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (64.77%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        int count = 1;
-        vector<vector<int>> res(n, vector<int>(n));
+        vector<vector<int>> matrix;
+        matrix.resize(n, vector<int>(n));
         
         int left = 0, right = n, top = 0, bottom = n;
+        int count = 1;
         
         while(left < right && top < bottom){
-            //top left to top right
-            for(int i=left; i<right; i++) res[top][i] = count++;
+            for(int i = left; i < right; i++) matrix[top][i] = count++;
             top++;
             
-            //top right to bottom right
-            for(int i=top; i<bottom; i++) res[i][right-1] = count++;
+            for(int i = top; i < bottom; i++) matrix[i][right-1] = count++;
             right--;
             
-            if(!(left < right && top < bottom)) break; 
+            if(!(left < right && top < bottom)) break;
             
-            //bottom right to bottom left
-            for(int i=right-1; i>=left; i--) res[bottom-1][i] = count++;
+            for(int i = right - 1; i >= left; i--) matrix[bottom-1][i] = count++;
             bottom--;
             
-            //bottom left to top left
-            for(int i=bottom-1; i>=top; i--) res[i][left] = count++;
+            for(int i = bottom - 1; i >= top; i--) matrix[i][left] = count++;
             left++;
+            
         }
         
-        return res;
+        return matrix;
     }
 };
