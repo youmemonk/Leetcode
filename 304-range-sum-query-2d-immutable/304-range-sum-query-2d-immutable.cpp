63c573b1a986af28291f5b278b5bf400ca5929ec
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2022-08-09 00:42:56 +0530

    Time: 527 ms (78.21%), Space: 148.7 MB (18.12%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -6,17 +6,17 @@ public:
         int n = matrix.size(), m = matrix[0].size();
         this->matrix = matrix;
         ps.resize(n, vector<int>(m));
-        calcPrefixSumMatrix(matrix);
+        calcPrefixSum(matrix);
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
         int toAdd = ps[row2][col2] + ((row1 != 0 && col1 != 0) ? ps[row1-1][col1-1] : 0);
-        int toSub = ((col1 != 0) ? ps[row2][col1-1] : 0) +( (row1 != 0) ? ps[row1-1][col2] : 0);
+        int toSub = ((col1 != 0) ? ps[row2][col1-1] : 0) + ((row1 != 0) ? ps[row1-1][col2] : 0);
         
         return (toAdd - toSub);
     }
     
-    void calcPrefixSumMatrix(vector<vector<int>> matrix){
+    void calcPrefixSum(vector<vector<int>> matrix){
         int n = matrix.size(), m = matrix[0].size();
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
