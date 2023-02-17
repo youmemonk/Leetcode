e9c73268888134a0e78a080225ee1857cd9af313
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2023-02-18 03:35:35 +0530

    Time: 140 ms (51.51%), Space: 69.4 MB (33.96%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,10 +4,9 @@ public:
         unordered_set<int> s;
         
         for(int i = 0; i < nums.size(); i++){
-            if(s.find(nums[i]) != s.end()) return true;
-            else s.insert(nums[i]);
+            if(s.find(nums[i]) == s.end()) s.insert(nums[i]);
+            else return true;
         }
-        
         return false;
     }
 };
