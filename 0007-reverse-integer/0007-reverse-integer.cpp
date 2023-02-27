ce6ebd95eddfd53dbe1366f716e6e183b9735566
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2023-02-27 19:45:40 +0530

    Time: 3 ms (57.11%), Space: 6 MB (12.65%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long int ans = 0, digit;
+        while( x!= 0 ){
+            digit = x % 10;
+            
+            if((ans > INT_MAX/10) || (ans < INT_MIN/10)){
+                return 0;
+            }
+            
+            ans = (ans * 10) + digit;
+            x = x/10;
+        }
+        return ans;
+    }
+};
