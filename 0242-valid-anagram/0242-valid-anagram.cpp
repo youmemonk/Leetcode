b0fac521e6a47130fca08239d50dfc20d881104b
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2023-07-10 03:54:21 +0530

    Time: 13 ms (54.63%), Space: 7.2 MB (80.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()) return false;
-        
-        unordered_map<char, int> sm, tm;
-        
-        for(int i = 0; i < s.length(); i++){
-            if(sm.find(s[i]) == sm.end()) sm.insert({s[i], 1});
-            else sm[s[i]]++;
-            
-            if(tm.find(t[i]) == tm.end()) tm.insert({t[i], 1});
-            else tm[t[i]]++;
-        }
-        
-        return (sm == tm);
+    if(s.length() != t.length()) return false;
+    unordered_map<char, int> sm, tm;
+
+    for(int i = 0; i < s.length(); i++){
+        if(sm.find(s[i]) != sm.end()) sm[s[i]]++;
+        else sm.insert({s[i], 1});
+
+        if(tm.find(t[i]) != tm.end()) tm[t[i]]++;
+        else tm.insert({t[i], 1});
+    }
+
+    return (sm == tm);
     }
 };
