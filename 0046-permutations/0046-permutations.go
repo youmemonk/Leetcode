7c9921eb1c28d36c96c22506aa3e20fad19c1bad
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2023-08-02 23:09:43 +0530

    Time: 2 ms (61.28%), Space: 2.6 MB (70.14%) - LeetHub

---
+++ 0046-permutations/0046-permutations.go
@@ -0,0 +1,31 @@
+func permute(nums []int) [][]int {
+    var res [][]int
+    
+    permutation := make([]int, len(nums))
+    visit := make([]bool, len(nums))
+    
+    var backtrack func(int)
+    backtrack = func(index int) {
+        if index == len(nums) {
+            copiedPermutation := make([]int, len(nums))
+            copy(copiedPermutation, permutation)
+            
+            res = append(res, copiedPermutation)
+            
+            return
+        }   
+        
+        for i := 0; i < len(nums); i++ {
+            if visit[i] == false {
+                visit[i] = true
+                permutation[index] = nums[i]
+                backtrack(index+1)   
+                visit[i] = false
+            }
+        }
+    }
+    
+    backtrack(0)
+    
+    return res
+}
