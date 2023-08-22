298fbfb3777e0332def8866d899b1c48e45a0b70
Mayank Ukey <70985854+youmemonk@users.noreply.github.com>
2023-08-23 03:54:54 +0530

    Time: 26 ms (90.79%), Space: 20.7 MB (38.83%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> hashmap;
-        
-        for(auto str: strs){
-            string sortedStr = str;
-            sort(sortedStr.begin(), sortedStr.end());
-            hashmap[sortedStr].push_back(str);
+
+        for(auto str:strs){
+            string sorted_str = str;
+            sort(sorted_str.begin(), sorted_str.end());
+            hashmap[sorted_str].push_back(str);
         }
-        
+
         vector<vector<string>> result;
-        for(auto x: hashmap) result.push_back(x.second);
-        
+        for(auto x:hashmap) result.push_back(x.second);
+
         return result;
     }
 };
